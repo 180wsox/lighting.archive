c14e90ccd69bb57bf2c5c19a00b25dfd26ad44aa
L. S. <209544553+GT1650CS@users.noreply.github.com>
2026-03-11 22:23:00 +0000

    Update header.php - add links page

--- header.php
+++ header.php
@@ -6,6 +6,8 @@ $title = $title ?? "lighting.archive";
   <div class="w3-bar w3-black">
     <a href="./index.html" class="w3-bar-item w3-button">Home</a>
     <a href="./datasheets.html" class="w3-bar-item w3-button">Datasheets</a>
+    <a href="./links.html" class="w3-bar-item w3-button">Links</a>
   </div>
 
 </header>
+
