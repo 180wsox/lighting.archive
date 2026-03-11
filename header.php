af8c252dde1f5a84ad04f7e4f74f76da3ae48266
L. S. <209544553+GT1650CS@users.noreply.github.com>
2026-03-11 22:04:28 +0000

    update header.php
    oops was referencing the domain root again lol

--- header.php
+++ header.php
@@ -4,6 +4,7 @@ $title = $title ?? "lighting.archive";
 
 <header>
   <div class="w3-bar w3-black">
-    <a href="/index.html" class="w3-bar-item w3-button">Home</a>
+    <a href="./index.html" class="w3-bar-item w3-button">Home</a>
   </div>
+
 </header>
