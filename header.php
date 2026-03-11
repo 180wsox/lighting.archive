92cc3b6d6e14c9a3a208db2b50e88b33452d1287
L. S. <209544553+GT1650CS@users.noreply.github.com>
2026-03-11 22:07:32 +0000

    update header.php - add datasheets page

--- header.php
+++ header.php
@@ -5,6 +5,7 @@ $title = $title ?? "lighting.archive";
 <header>
   <div class="w3-bar w3-black">
     <a href="./index.html" class="w3-bar-item w3-button">Home</a>
+    <a href="./datasheets.html" class="w3-bar-item w3-button">Datasheets</a>
   </div>
 
 </header>
